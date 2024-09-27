b91261af317b4a865b8fc05fa12064c61e22f3f4
val-makkas <chrysovm@csd.auth.gr>
2024-09-27 12:54:19 +0300

    updated build

---
+++ build/src/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const diagnosesRouter_1 = __importDefault(require("./routes/diagnosesRouter"));
+const patientsRouter_1 = __importDefault(require("./routes/patientsRouter"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+const PORT = 3000;
+app.get('/api/ping', (_req, res) => {
+    console.log('ping');
+    res.send('pong');
+});
+app.use('/api/diagnoses', diagnosesRouter_1.default);
+app.use('/api/patients', patientsRouter_1.default);
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
